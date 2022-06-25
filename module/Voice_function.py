4a06643f79e33954fb553585003aa8ddd7256933
kglory <64764153+kgloryi@users.noreply.github.com>
2022-06-25 20:20:45 +0800

    Update Voice_function.py

--- module/Voice_function.py
+++ module/Voice_function.py
@@ -12,14 +12,14 @@ class Voice():
         print("""
 语种列表:https://ai.baidu.com/ai-doc/MT/4kqryjku9
 """)
-        voicetype = input("Please enter the voice generation type（请输入语音生成类型）:")
+        voicetype = input("Please enter the voice generation type（请输入翻译后的语音类型）:")
         client = AipSpeech(self.APP_ID, self.API_KKY, self.SECRET_KEY)
         dataread = open(os.getcwd()+"\\data\\translationdatabase.txt","r+").read().split("\n")
         for x in range(len(dataread)-1):
             fenge = dataread[x].split(">")[1] #翻译的结果
             result  = client.synthesis(fenge,voicetype, 1, {'vol': 5,})
             if not isinstance(result, dict): #开始分批生成语音
-                with open(os.getcwd()+'/music/audio'+str(x)+'.mp3', 'wb') as f:
+                with open(os.getcwd()+'/music/audio'+str(x)+'.mp3', 'w+b') as f:
                     f.write(result)
             f.close()
         print("\nSpeech synthesis complete（语音合成完毕,请查看当前目录下的music目录）")
